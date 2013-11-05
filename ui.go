881856421e41df1bf1ee8d3119b7f05d8ce284dd
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-11-04 17:19:33 -0800

    Ask should return immediately if interrupted

--- ui.go
+++ ui.go
@@ -1,8 +1,11 @@
 package cli
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"os"
+	"os/signal"
 )
 
 // Ui is an interface for interacting with the terminal, or "interface"
@@ -38,9 +41,37 @@ func (u *BasicUi) Ask(query string) (string, error) {
 		return "", err
 	}
 
-	var line string
-	_, err := fmt.Fscanln(u.Reader, &line)
-	return line, err
+	// Register for interrupts so that we can catch it and immediately
+	// return...
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+	defer signal.Stop(sigCh)
+
+	// Ask for input in a go-routine so that we can ignore it.
+	errCh := make(chan error, 1)
+	lineCh := make(chan string, 1)
+	go func() {
+		var line string
+		if _, err := fmt.Fscanln(u.Reader, &line); err != nil {
+			errCh <- err
+			return
+		}
+
+		lineCh <- line
+	}()
+
+	select {
+	case err := <-errCh:
+		return "", err
+	case line := <-lineCh:
+		return line, nil
+	case <-sigCh:
+		// Print a newline so that any further output starts properly
+		// on a new line.
+		fmt.Fprintln(u.Writer)
+
+		return "", errors.New("interrupted")
+	}
 }
 
 func (u *BasicUi) Error(message string) {
