ce8d361e2f65d20a7c6e9c17e95a180ac863e4e4
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-11-04 16:16:19 -0800

    Add Ui interfaces

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Go CLI Library
 
-cli is a library for implementing sub-command based command-line interfaces
-in Go. cli is the library that powers the CLI for
+cli is a library for implementing powerful command-line interfaces in Go.
+cli is the library that powers the CLI for
 [Packer](https://github.com/mitchellh/packer) and
 [Serf](https://github.com/hashicorp/serf).
+
+## Features
+
+* Easy sub-command based CLIs: `cli foo`, `cli bar`, etc.
+
+* Automatic help generation for listing subcommands
+
+* Automatic help flag recognition of `-h`, `--help`, etc.
+
+* Use of Go interfaces/types makes augmenting various parts of the library a
+  piece of cake.
---
+++ ui.go
@@ -0,0 +1,76 @@
+package cli
+
+import (
+	"fmt"
+	"io"
+	"log"
+)
+
+// Ui is an interface for interacting with the terminal, or "interface"
+// of a CLI. This abstraction doesn't have to be used, but helps provide
+// a simple, layerable way to manage user interactions.
+type Ui interface {
+	// Output is called for normal standard output.
+	Output(string)
+
+	// Info is called for information related to the previous output.
+	// In general this may be the exact same as Output, but this gives
+	// Ui implementors some flexibility with output formats.
+	Info(string)
+
+	// Error is used for any error messages that might appear on standard
+	// error.
+	Error(string)
+}
+
+// BasicUi is an implementation of Ui that just outputs to the given
+// writer.
+type BasicUi struct {
+	Writer io.Writer
+}
+
+func (u *BasicUi) Error(message string) {
+	fmt.Fprint(u.Writer, message)
+	fmt.Fprint(u.Writer, "\n")
+}
+
+func (u *BasicUi) Info(message string) {
+	u.Output(message)
+}
+
+func (u *BasicUi) Output(message string) {
+	fmt.Fprint(u.Writer, message)
+	fmt.Fprint(u.Writer, "\n")
+}
+
+// PrefixedUi is an implementation of Ui that prefixes messages.
+type PrefixedUi struct {
+	OutputPrefix string
+	InfoPrefix   string
+	ErrorPrefix  string
+	Ui           Ui
+}
+
+func (u *PrefixedUi) Error(message string) {
+	if message != "" {
+		message = fmt.Sprintf("%s%s", u.ErrorPrefix, message)
+	}
+
+	u.Ui.Error(message)
+}
+
+func (u *PrefixedUi) Info(message string) {
+	if message != "" {
+		message = fmt.Sprintf("%s%s", u.InfoPrefix, message)
+	}
+
+	u.Ui.Info(message)
+}
+
+func (u *PrefixedUi) Output(message string) {
+	if message != "" {
+		message = fmt.Sprintf("%s%s", u.OutputPrefix, message)
+	}
+
+	u.Ui.Output(message)
+}
