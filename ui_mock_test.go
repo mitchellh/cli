fb74e2d8a800fdbbcab65624afb9dbd47da7a527
Milad Irannejad <moorara@gmail.com>
2017-12-25 01:06:23 -0500

    Fix discrepancy between BasicUi and MockUi

--- ui_mock_test.go
+++ ui_mock_test.go
@@ -1,9 +1,44 @@
 package cli
 
 import (
+	"io"
 	"testing"
 )
 
 func TestMockUi_implements(t *testing.T) {
 	var _ Ui = new(MockUi)
+}
+
+func TestMockUi_Ask(t *testing.T) {
+	tests := []struct {
+		name           string
+		query, input   string
+		expectedResult string
+	}{
+		{"EmptyString", "Middle Name?", "\n", ""},
+		{"NonEmptyString", "Name?", "foo bar\nbaz\n", "foo bar"},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			in_r, in_w := io.Pipe()
+			defer in_r.Close()
+			defer in_w.Close()
+
+			ui := &MockUi{
+				InputReader: in_r,
+			}
+
+			go in_w.Write([]byte(tc.input))
+
+			result, err := ui.Ask(tc.query)
+			if err != nil {
+				t.Fatalf("err: %s", err)
+			}
+
+			if result != tc.expectedResult {
+				t.Fatalf("bad: %#v", result)
+			}
+		})
+	}
 }
